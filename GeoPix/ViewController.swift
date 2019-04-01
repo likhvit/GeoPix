5fddf9f78fc72bb6c8ecf8bd2c7d795ce63ab678
Andreas Christou <s5109900@bournemouth.ac.uk>
2019-04-01 13:00:15 +0100

    pin update

--- GeoPix/ViewController.swift
+++ GeoPix/ViewController.swift
@@ -6,33 +6,24 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+   
+        var mapView:MKMapView!
+    
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-<<<<<<< HEAD
-//<<<<<<< HEAD
+
         
-        
-        
-//=======
-=======
-        
-        //alex comment
-        
-        
->>>>>>> b3f313fd9d16183aa99317b824fadf4bbfe37282
+
         locationManager.requestAlwaysAuthorization()
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
 
     
-<<<<<<< HEAD
-//>>>>>>> 12222787e7197ffcf316dc12ef1a8ac6840e8dfe
-=======
 
->>>>>>> b3f313fd9d16183aa99317b824fadf4bbfe37282
     }
 
 
@@ -42,4 +33,6 @@ extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         print(locations.last!)
     }
+}
+
 }
--- GeoPix/secondViewController.swift
+++ GeoPix/secondViewController.swift
@@ -2,3 +2,8 @@ import Foundation
 import UIKit
 import CoreLocation
 import MapKit
+
+
+protocol SecondViewControllerDelegate {
+    func didUpdate (annnotation: CustomAnnotation)
+}
