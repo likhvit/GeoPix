e63efe1d604c484534f808a558da79e5895c8ad8
Kasper Bugge Kaspersen (s5120396) <s5120396@mac38571.mac.bournemouth.ac.uk>
2019-04-01 12:21:24 +0100

    Removing comments + import mapit

--- GeoPix/ViewController.swift
+++ GeoPix/ViewController.swift
@@ -1,18 +1,12 @@
-//
-//  ViewController.swift
-//  GeoPix
-//
-//  Created by Kasper Bugge Kaspersen (s5120396) on 01/04/2019.
-//  Copyright © 2019 Kasper Bugge Kaspersen (s5120396). All rights reserved.
-//
-
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
 
