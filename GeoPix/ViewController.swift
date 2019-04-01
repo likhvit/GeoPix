6f6e6da2f4a87dfd95478f07d769df8ced4df51a
Alex Bessell (s5113132) <s5113132@mac38566.mac.bournemouth.ac.uk>
2019-04-01 12:42:56 +0100

    added second view controller
    added second view controller for the image view

--- GeoPix/ViewController.swift
+++ GeoPix/ViewController.swift
@@ -11,17 +11,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-<<<<<<< HEAD
-        //alex comment
+//<<<<<<< HEAD
         
         
-=======
+        
+//=======
         locationManager.requestAlwaysAuthorization()
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
 
     
->>>>>>> 12222787e7197ffcf316dc12ef1a8ac6840e8dfe
+//>>>>>>> 12222787e7197ffcf316dc12ef1a8ac6840e8dfe
     }
 
 
