ef048b6f97ecca08c5b8457fd30a1a93c6e80e0a
Alex Bessell (s5113132) <s5113132@mac38566.mac.bournemouth.ac.uk>
2019-04-01 12:53:50 +0100

    fixed viewcontroller

--- GeoPix/ViewController.swift
+++ GeoPix/ViewController.swift
@@ -11,33 +11,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-<<<<<<< HEAD
-//<<<<<<< HEAD
+
         
-        
-        
-//=======
-=======
-        
-        //alex comment
-        
-        
->>>>>>> b3f313fd9d16183aa99317b824fadf4bbfe37282
         locationManager.requestAlwaysAuthorization()
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
 
-    
-<<<<<<< HEAD
-//>>>>>>> 12222787e7197ffcf316dc12ef1a8ac6840e8dfe
-=======
-
->>>>>>> b3f313fd9d16183aa99317b824fadf4bbfe37282
-    }
 
 
 }
 
+    
+}
 extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         print(locations.last!)
