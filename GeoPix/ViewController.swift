12222787e7197ffcf316dc12ef1a8ac6840e8dfe
Kasper Bugge Kaspersen (s5120396) <s5120396@mac38571.mac.bournemouth.ac.uk>
2019-04-01 12:30:30 +0100

    Added Location manager

--- GeoPix/ViewController.swift
+++ GeoPix/ViewController.swift
@@ -1,15 +1,28 @@
 import UIKit
+import CoreLocation
 import MapKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+
+    
     }
 
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations.last!)
+    }
+}
