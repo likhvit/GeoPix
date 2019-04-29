9145fbf5f1d32894b1f6f95c709e63a160f59b9c
Andreas Christou <s5109900@bournemouth.ac.uk>
2019-04-29 13:11:02 +0100

    image viewcontroller

--- GeoPix/UploadViewController.swift
+++ GeoPix/UploadViewController.swift
@@ -1,8 +1,11 @@
 import UIKit
 import Firebase
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
+    @IBOutlet weak var imagePicker: UIImageView!
+    @IBOutlet weak var imageDescription: UILabel!
+    @IBOutlet weak var uploadDate: UILabel!
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
@@ -18,5 +21,33 @@ class UploadViewController: UIViewController {
         
     }
     
+    @IBAction func importButton(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            image.sourceType = .camera
+            self.present (image, animated: true, completion: nil)
+        } else {
+            print("camera not available")
+        }
+        
+        image.sourceType =
+            UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = false
+        self.present(image, animated: true){
+            
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imagePicker.image = image
+        } else{
+            //error
+        }
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+
     
 }
