3246347285920a549cc6df4791664591730157f7
Alex Bessell (s5113132) <s5113132@mac42942.mac.bournemouth.ac.uk>
2019-05-03 09:40:01 +0100

    removed comments in viewcontroller
    removed old commented code

--- GeoPix/ViewController.swift
+++ GeoPix/ViewController.swift
@@ -22,14 +22,9 @@ class ViewController: UIViewController {
         locationManager.startUpdatingLocation()
 
         let coordinate = CLLocationCoordinate2D(latitude: 50.716088, longitude: -1.874064)
-        //mapView.setRegion(region, animated: true)
         let region = CLCircularRegion(center: coordinate, radius: 300, identifier: "One")
         locationManager.startMonitoring(for: region)
-        
-        //let span = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
-        //let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50.721972, longitude: -1.873795), span: span)
-        //sets the map to the Region and it is animated as it moves to it
-        //mapView.setRegion(region, animated: true)
+    
         
         func loadLocations() {
             let ref = Firestore.firestore().collection("locations")
